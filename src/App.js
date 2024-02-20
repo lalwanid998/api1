faee132e44f68f268daa746387efa4b82593d8c1
lalwanid998 <lalwanid998@gmail.com>
2024-02-20 15:17:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {mydata:[]  };
+  }
+  componentDidMount() {
+    axios.get("https://dummyjson.com/products")
+    .then(res=>{
+      console.table(res.data.products);
+      this.setState({mydata:res.data.products})
+    })
+  }
+  render() { 
+    return ( 
+      <>
+      <h2>API Demo</h2>
+      <table border={1}>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Price</th>
+            <th>Image</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.state.mydata.map((value,index)=>{
+          return <tr>
+            <td>{value.id}</td>
+            <td>{value.title}</td>
+            <td>{value.price}</td>
+            <td><img src={value.thumbnail} width={100}/></td>
+          </tr>
+        })}
+      </tbody>
+      </table>
+      </>
+     );
+  }
 }
-
+ 
 export default App;
